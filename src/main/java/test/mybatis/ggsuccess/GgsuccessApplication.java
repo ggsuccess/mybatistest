81a9cf3af5a72b3f6d862f2499237a96d059d307
[ggsuccess] <[ggsuccess@naver.com]>
2023-01-11 18:32:07 +0900

    first init

---
+++ src/main/java/test/mybatis/ggsuccess/GgsuccessApplication.java
@@ -0,0 +1,13 @@
+package test.mybatis.ggsuccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgsuccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgsuccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/mybatis/ggsuccess/GgsuccessApplicationTests.java
@@ -0,0 +1,13 @@
+package test.mybatis.ggsuccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgsuccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
